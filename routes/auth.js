3a84f303f66bfeba9ea08ffaa67b3a3f3b257e2b
tae <aipooh8882@naver.com>
2021-03-02 23:36:06 +0900

    views, passports pushed

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,13 @@ const express = require('express');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 
-const{ isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const User = require('../models.user');
 
 const router = express.Router();
 
 router.post('/join', isNotLoggedIn, async (req, res, next) => {
-    const {email, nick, password, money} = req.body;
+    const { email, nick, password, money } = req.body;
     try {
         const exUser = await User.findOne({ where: { email } });
         if (exUser) {
@@ -29,20 +29,20 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
 
 router.post('/login', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local', (authError, user, info) => {
-      if (authError) {
-        console.error(authError);
-        return next(authError);
-      }
-      if (!user) {
-        return res.redirect(`/?loginError=${info.message}`);
-      }
-      return req.login(user, (loginError) => {
-        if (loginError) {
-          console.error(loginError);
-          return next(loginError);
+        if (authError) {
+            console.error(authError);
+            return next(authError);
         }
-        return res.redirect('/');
-      });
+        if (!user) {
+            return res.redirect(`/?loginError=${info.message}`);
+        }
+        return req.login(user, (loginError) => {
+            if (loginError) {
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        });
     })(req, res, next);
 });
 
