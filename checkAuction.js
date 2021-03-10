fd33546a9134b785b99e0f5ddeb660d30e7afe15
tae <aipooh8882@naver.com>
2021-03-11 00:52:02 +0900

    2021-03-11

--- checkAuction.js
+++ checkAuction.js
@@ -1,4 +1,5 @@
 const { Op } = require('Sequelize');
+const schedule = require('node-schedule');
 
 const { Good, Auction, User, sequelize } = require('./models');
 
@@ -6,7 +7,7 @@ module.exports = async () => {
     console.log('checkAuction');
     try{
         const yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 1); 
+        yesterday.setDate(yesterday.getDate() - 1); // 어제 시간
         const targets = await Good.findAll({
             where: {
                 SoldId: null,
@@ -24,6 +25,28 @@ module.exports = async () => {
             }, {
                 where: { id: success.UserId },
             });
+        });
+        const unsold = await Good.findAll({
+            where: {
+                SoldId: null,
+                createdAt: { [Op.gt]: yesterday },
+            },
+        });
+        unsold.forEach((target) => {
+            const end = new Date(unsold.createdAt);
+            end.setDate(end.getDate() + 1);
+            schedule.scheduleJob(end, async () => {
+                const success = await Auction.findOne({
+                    where: { GoodId: target.id },
+                    order: [['bid', 'DESC']],
+                });
+                await Good.update({ SoldId: success.UserId }, { where: { id: target.id } });
+                await User.update({
+                    money: sequelize.literal(`money - ${success.bid}`),
+                }, {
+                    where: { id: success.UserId },
+                });
+            })
         });
     } catch (error) {
         console.error(error);
